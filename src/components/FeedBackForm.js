cb472503a18a1880748e53f4cc5f933d471eddd6
ANDREWS RAY DE ASSUNCAO VIEIRA <andrews_vieira@estudante.sc.senai.br>
2022-10-05 21:20:03 -0300

    fomulário inicial - incompleto

---
+++ src/components/FeedBackForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import {
+  Form,
+  Input,
+  Button,
+  Select,
+} from 'antd';
+const { TextArea } = Input;
+
+const onSave = (value) => console.log(value);
+
+const FeedBackForm = () => {
+  return (
+    <>
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="vertical"
+      >
+        <Form.Item label="Tipo de feedback">
+          <Select>
+            <Select.Option value="Normal">Normal</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label="Feedback">
+          <TextArea rows={4} />
+        </Form.Item>
+        <Form.Item>
+          <Button onSave = {onSave(this)}>Enviar</Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+export default () => <FeedBackForm />;
